98ffe8d0f7efed37703f63bdc745902ad417da4e
Krishna Prem Pasumarthy <36132355+kpasumar@users.noreply.github.com>
2022-01-17 18:18:45 -0500

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -3,6 +3,7 @@ SELECT Shippers.ShipperName,COUNT(OrderID) AS Total FROM Orders
 LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
 WHERE Shippers.ShipperName = 'Speedy Express'
 GROUP BY Shippers.ShipperName
+-- Answer: 54
 
 -- Part b
 SELECT TOP 1 Employees.LastName,COUNT(Orders.EmployeeID) AS Total FROM Employees
@@ -10,6 +11,7 @@ RIGHT JOIN Orders
 ON Employees.EmployeeID = Orders.EmployeeID
 GROUP BY Employees.LastName
 ORDER BY 2 DESC
+-- Answer: Peacock
 
 -- Part c
 SELECT TOP 1 ProductName,COUNT(ProductName) AS Total FROM (SELECT * FROM (SELECT * FROM (SELECT * FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID WHERE c.Country = 'Germany') T1
@@ -17,4 +19,5 @@ LEFT JOIN OrderDetails od ON T1.OrderID = od.OrderID) T2
 LEFT JOIN Products p ON T2.ProductID = p.ProductID) T3
 GROUP BY ProductName
 ORDER BY 2 DESC
+-- Answer: Gorgonzola Telino
 
