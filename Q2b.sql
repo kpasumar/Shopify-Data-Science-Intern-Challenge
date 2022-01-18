17c0ad287924f65654de8208582f7ed3a404e4fb
Krishna Prem Pasumarthy <36132355+kpasumar@users.noreply.github.com>
2022-01-17 19:14:31 -0500

    Update Q2b.sql

--- Q2b.sql
+++ Q2b.sql
@@ -1,1 +1,5 @@
-
+SELECT TOP 1 Employees.LastName,COUNT(Orders.EmployeeID) AS Total FROM Employees
+RIGHT JOIN Orders 
+ON Employees.EmployeeID = Orders.EmployeeID
+GROUP BY Employees.LastName
+ORDER BY 2 DESC
