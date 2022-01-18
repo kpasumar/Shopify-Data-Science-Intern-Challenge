8880e75e94d93fd156edea537650003f854fa33c
Krishna Prem Pasumarthy <36132355+kpasumar@users.noreply.github.com>
2022-01-17 19:14:03 -0500

    Update Q2a.sql

--- Q2a.sql
+++ Q2a.sql
@@ -5,14 +5,6 @@ WHERE Shippers.ShipperName = 'Speedy Express'
 GROUP BY Shippers.ShipperName
 -- Answer: 54
 
--- Part b
-SELECT TOP 1 Employees.LastName,COUNT(Orders.EmployeeID) AS Total FROM Employees
-RIGHT JOIN Orders 
-ON Employees.EmployeeID = Orders.EmployeeID
-GROUP BY Employees.LastName
-ORDER BY 2 DESC
--- Answer: Peacock
-
 -- Part c
 SELECT TOP 1 ProductName,COUNT(ProductName) AS Total FROM (SELECT * FROM (SELECT * FROM (SELECT * FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID WHERE c.Country = 'Germany') T1
 LEFT JOIN OrderDetails od ON T1.OrderID = od.OrderID) T2
