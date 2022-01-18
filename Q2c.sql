cd8897560d43c95ca7069e80be90a7624a866b2b
Krishna Prem Pasumarthy <36132355+kpasumar@users.noreply.github.com>
2022-01-17 19:15:30 -0500

    Update Q2c.sql

--- Q2c.sql
+++ Q2c.sql
@@ -1,1 +1,5 @@
-
+SELECT TOP 1 ProductName,COUNT(ProductName) AS Total FROM (SELECT * FROM (SELECT * FROM (SELECT * FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID WHERE c.Country = 'Germany') T1
+LEFT JOIN OrderDetails od ON T1.OrderID = od.OrderID) T2
+LEFT JOIN Products p ON T2.ProductID = p.ProductID) T3
+GROUP BY ProductName
+ORDER BY 2 DESC
