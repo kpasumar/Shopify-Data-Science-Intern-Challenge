0c16c7a01e3826fe044033acf663327034d7e58f
Krishna Prem Pasumarthy <36132355+kpasumar@users.noreply.github.com>
2022-01-17 19:15:14 -0500

    Update Q2a.sql

--- Q2a.sql
+++ Q2a.sql
@@ -1,15 +1,5 @@
--- Part a
+
 SELECT Shippers.ShipperName,COUNT(OrderID) AS Total FROM Orders
 LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
 WHERE Shippers.ShipperName = 'Speedy Express'
 GROUP BY Shippers.ShipperName
--- Answer: 54
-
--- Part c
-SELECT TOP 1 ProductName,COUNT(ProductName) AS Total FROM (SELECT * FROM (SELECT * FROM (SELECT * FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID WHERE c.Country = 'Germany') T1
-LEFT JOIN OrderDetails od ON T1.OrderID = od.OrderID) T2
-LEFT JOIN Products p ON T2.ProductID = p.ProductID) T3
-GROUP BY ProductName
-ORDER BY 2 DESC
--- Answer: Gorgonzola Telino
-
